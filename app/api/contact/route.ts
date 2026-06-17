65fe3b8657c145ed015b2501db0023ef9fe2f6f4
gulshan <gulshany7669@gmail.com>
2026-06-17 12:27:40 +0530

    update the mail service

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
     }
 
    const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
